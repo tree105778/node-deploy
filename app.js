81139bcfdca2ad7fba0d2b9149c4c7030b1c80f6
tree105778 <bjyon0415@gmail.com>
2024-03-01 00:55:27 +0900

    fourth commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
+const RedisStore = require('connect-redis').default;
 
 dotenv.config();
 const redisClient = redis.createClient({
